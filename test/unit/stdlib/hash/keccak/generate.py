34bb58ea126ccf3bfc2775b2c3cc53697d730c3f
Zack Puhl <github@xmit.xyz>
2025-08-19 21:18:20 +0000

    wip: completed cSHAKE and KMAC testing; all passing

--- test/unit/stdlib/hash/keccak/generate.py
+++ test/unit/stdlib/hash/keccak/generate.py
@@ -28,6 +28,11 @@ sample_custom_strs = [
 	"my customization string that is a bit longer"
 ]
 
+sample_kmac_keys = [
+	'correct horse battery staple cryptographic brainrot',
+	'RJ5r5ToA9N9~Y-,kin8y]xB}kviaDk_kPI'
+]
+
 
 def keccak_digest(bits, data):
 	# print(f"KECCAK INPUT (len: {len(data)}): '{data}'")
@@ -82,10 +87,23 @@ def get_xofs(levels, trim_lengths, customization_strings, xof_func):
 
 
 def kmac(level, key, trim_length, data, custom = None):
-	return "abc"
+	print(f"kmac (k:{key} / c:{custom} / L:{level} / l:{trim_length} / D:{len(data)})")
+	xof = eval(f"KMAC{level}.new(key=b'{key}', mac_len={trim_length}" + (f", custom=b'{custom}'" if custom is not None else "") + ")")
+	xof.update(data.encode('utf-8'))
+	return xof.hexdigest()
 
-def get_kmacs():
-	return {}   # TODO
+def get_kmacs(levels, trim_lengths, keys, customization_strings, kmac_func):
+	return [kmac_func(l, k, i, j, s)  for j in simple[0:2]  for k in keys for s in customization_strings for l in levels  for i in trim_lengths  ];
+
+	res = dict()
+	for level in levels:
+		res[level] = {
+			# 'empty':	[kmac_func(level, k, i, "", s) for i in trim_lengths for k in keys for s in customization_strings],
+			'simple':	[kmac_func(level, k, i, j, s) for j in simple[0:2] for i in trim_lengths for k in keys for s in customization_strings ],
+			# 'slow':		[kmac_func(level, k, i, "".join(['a'] * 100000000), s) for i in trim_lengths for k in keys for s in customization_strings],
+			# 'a...':		[kmac_func(level, k, i, "".join(list_of_a[0:(1+j*64)]), s) for j in range(5) for i in trim_lengths for k in keys for s in customization_strings],
+		}
+	return res
 
 
 def main():
@@ -94,8 +112,8 @@ def main():
 		# 'SHA3':			get_digests([224, 256, 384, 512], sha3_digest),
 		# 'SHAKE':		get_xofs([128, 256], [16, 256], [''], shake_xof),
 		# 'TURBO-SHAKE':	get_xofs([128, 256], [16, 256], [''], turboshake_xof),
-		'CSHAKE':		get_xofs([128, 256], [64], sample_custom_strs, cshake_xof),
-		# 'KMAC':			get_kmacs(),
+		# 'CSHAKE':		get_xofs([128, 256], [64], sample_custom_strs, cshake_xof),
+		'KMAC':			get_kmacs([128, 256], [32, 64, 128], sample_kmac_keys, sample_custom_strs, kmac),
 	}
 	pprint.pprint(res)
 
